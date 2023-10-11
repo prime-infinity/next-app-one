d1219799dd66e4a175cf17cf8c03b65b0966daf1
prime-infinity <soe.a.prime@gmail.com>
2023-10-11 15:51:27 -0700

    styles

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "./dev-img.png";
 
 export default function Navbar() {
   return (
     <nav>
+      <Image src={Logo} alt="img-logo" width={50} placeholder="blur" />
       <h1>Dojo Helpdesk</h1>
-      <Link href={"/"}>Dashboardd</Link>
+      <Link href={"/"}>Dashboard</Link>
       <Link href={"/tickets"}>Tickets</Link>
     </nav>
   );
