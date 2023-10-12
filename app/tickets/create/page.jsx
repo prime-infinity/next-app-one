7efa452b273870af10e599edc7c0ee32c1fc5060
prime-infinity <soe.a.prime@gmail.com>
2023-10-12 13:01:23 -0700

    client component

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,10 @@
+import CreateForm from "./createForm";
+
+export default function CreateTicket() {
+  return (
+    <main>
+      <h2 className="text-primary text-center">Add a new ticket</h2>
+      <CreateForm />
+    </main>
+  );
+}
