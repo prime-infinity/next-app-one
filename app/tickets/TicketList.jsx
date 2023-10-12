ee64ac8bbd85ca17723c1ee94dc13dbabd7561cf
prime-infinity <soe.a.prime@gmail.com>
2023-10-12 02:03:59 -0700

    getting data and cacheing in server components

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,5 +1,9 @@
 async function getTickets() {
-  const res = await fetch("http://localhost:4000/tickets");
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: {
+      revalidate: 3, //use 0 to stop cacheing
+    },
+  });
   return res.json();
 }
 
